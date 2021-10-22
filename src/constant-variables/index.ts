23f6e79d195f67c98ede1e53ffb8fc33f8dd44bf
Yarmolenko <yarmolenko.dany@gmail.com>
2021-10-22 11:36:20 +0300

    refactor: update email

--- src/constant-variables/index.ts
+++ src/constant-variables/index.ts
@@ -1,4 +1,4 @@
-export const EMAIL = 'info@glbconst.com'
+export const EMAIL = 'info@global-construction.com.ua'
 export const PHONE_NUMBER = '+380981034444'
 export const FORMATTED_PHONE_NUMBER = '+380 98 103 4444'
 export const ADDRESS = 'Ukraine Kyiv, 12A Zhylyanska str.'
